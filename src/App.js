02b252250d7d6f97b4d5f5e7f089389963fec0b0
Gautham Vijayan <kinggautham495@gmail.com>
2021-08-18 14:43:02 +0530

    Different Types of Data types

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  let city = "Paris";
 
-  let city = "Paris" 
+  // numbers
+  // Strings
+  // Booleans --> False True
+  // Array
+  // Objects
 
   // States in React is immutable
 
-
   const [name, setName] = useState("Javascript Developer");
   const [age, setAge] = useState(45);
+
+  const [isDifficult, setIsDifficult] = useState(false);
+
+  const [array, setArray] = useState(["1", "2", "3"]);
+
+  const [objects, setObjects] = useState({ name: "lion" });
+
   return (
     <div className="App">
       Gautham
