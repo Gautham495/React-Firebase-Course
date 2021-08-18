a74242a020fe6b47368fef2aff02725a98552d0a
Gautham Vijayan <kinggautham495@gmail.com>
2021-08-18 14:25:26 +0530

    Cleaning Up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-import "./App.css";
+import './App.css';
+
 
 function App() {
-  const [name, setName] = useState("Javascript Developer");
-  const [age, setAge] = useState(45);
   return (
     <div className="App">
-      Gautham
-      {name}
-      <div>{age}</div>
+     Gautham
     </div>
   );
 }
