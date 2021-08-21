1a46eaf5977516a93282c509ace7632f1050909f
Gautham Vijayan <kinggautham495@gmail.com>
2021-08-21 15:30:19 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,93 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  let city = "Paris";
+  // lifecycle
 
-  // numbers
-  // Strings
-  // Booleans --> False True
-  // Array
-  // Objects
+  const [message, setMessage] = useState("Hello");
 
-  // States in React is immutable
+  useEffect(() => {
+    // alert(message)
+  }, []); // empty dependency array
 
-  const [name, setName] = useState("Javascript Developer");
+  useEffect(() => {
+    alert("Changed");
+  }, [message]); // dependency Array
 
-  const [age, setAge] = useState(45);
+  // 1. When the page loads/ mounts for the first time lets alert
+  // the user "hello"
 
-  const [isDifficult, setIsDifficult] = useState(false);
-
-  const [onlyTrue, setOnlyTrue] = useState(true);
-
-  const [array, setArray] = useState(["1", "2", "3"]);
-
-  const [objects, setObjects] = useState({ name: "lion" });
-
-  const [arrobj, setArrobj] = useState([
-    { name: "lion", id: 1 },
-    { name: "tiger", id: 2 },
-    { name: "fish", id: 3 },
-    { name: "bird", id: 4 },
-    { name: "spaceship", id: 5 },
-  ]);
-
-  // Map functions is only used for arrays not for any other data types like objects
-
-  // map filter sort reduce
-
-  // Objects --> { name : "Tiger", age : 30 }
-  // key     // value
-
-  // 1. onClick
-  // 2. onChange
-
-  function Change() {}
-
-  const arrowFunction = () => {
-    if (isDifficult) {
-    }
-
-    // isAuthenticated  false --> true
-  };
+  // 2. When some variables or some functions get fired we can
+  // call the useEffect
 
   return (
     <div className="App">
-      Name : {name}
-      <div>{age}</div>
-      <button onClick={() => setName("React JS Developer")}>Change Name</button>
       <input
         type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-      ></input>
-      <button onClick={() => setIsDifficult(!isDifficult)}>
-        {" "}
-        Change Difficulty{" "}
-      </button>
-      {isDifficult ? (
-        <div> Its really Difficult </div>
-      ) : (
-        <div> Its not really that difficult </div>
-      )}
-      {onlyTrue && <div>It is really true</div>}
-      <div>
-        {arrobj.map((activity) => {
-          return (
-            <div key={activity.id}>
-              <div>{activity.name}</div>
-            </div>
-          );
-        })}
-      </div>
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      />
     </div>
   );
 }
 
 export default App;
-
-// false --> true  true ---> false
-
-
-// images, api files, components, styles
