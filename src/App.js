7783db0a24b856882ed5a7d8c5e3d4d309172aea
Gautham Vijayan <kinggautham495@gmail.com>
2021-08-21 14:44:51 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-
-
+import "./App.css";
+import Home from "./Home";
+import About from './About'
 function App() {
+  
+  const home = "House"
+  const pi = 3.14
+
+  const message = "Welcome this is about page"
+
+
   return (
     <div className="App">
-     Gautham
+      Gautham
+      <Home home={home} pi = {pi}   />
+      <About message = {message} />
     </div>
   );
 }
 
 export default App;
+
+// HTML tags
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = (props) => {
+    return (
+        <div>
+
+            Child component Called Home and {props.home}
+
+             and pi {props.pi}
+            
+        </div>
+    )
+}
+
+export default Home
