8822985a7ee5c5cae381378f32bff1d9aa1ec64d
Gautham Vijayan <kinggautham495@gmail.com>
2021-08-23 10:40:01 +0530

    showing firestore data to frontend

--- src/Components/Firestore.js
+++ src/Components/Firestore.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from "react";
-import 'firebase/firestore'
-import {db} from '../Firebase'
+import React, { useState, useEffect } from "react";
+import "firebase/firestore";
+import { db } from "../Firebase";
 
 const Firestore = () => {
+  const [nameData, setNameData] = useState([]);
+
   const getData = async () => {
+    let nameArray = [];
+
     const names = await db.collection("names").get();
+
     names.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
+      // console.log(doc.id, '=>', doc.data());
+      nameArray.push(doc.data());
     });
+
+    setNameData(nameArray);
+
+    console.log(nameData);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  return <div>Firebase Firestore Example</div>;
+  return (
+    <div>
+      Firebase Firestore Example
+      <div>
+        {nameData.map((doc) => {
+          return <div>{doc.name}</div>;
+        })}{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Firestore;
